dfc5ddc427c610217cc07a81020631a434db37c3
PlamoPhotos <plamodb@gmail.com>
2023-04-13 23:21:24 -0700

    Add a label to the record.

--- src/components/panes/MediaPlayerPane/utilities/rendering.utilities.ts
+++ src/components/panes/MediaPlayerPane/utilities/rendering.utilities.ts
@@ -1,6 +1,9 @@
 // #region Rendering Constants
 
-
+const VINYL_COLOR : string          = '#3e3e3f';
+const LABEL_COLOR : string          = 'white';
+const SPINDLE_COLOR : string        = 'black';
+const DIVIDING_LINE_COLOR : string  = '#171717';
 
 // #endregion Rendering Constants
 
@@ -25,7 +28,7 @@ export function renderVinyl(context : CanvasRenderingContext2D, radius : number)
   // Outer circle:
   context.beginPath();
   context.arc(radius, radius, radius, 0, 2 * Math.PI);
-  context.fillStyle = '#3e3e3f';
+  context.fillStyle = VINYL_COLOR;
   context.fill();
 }
 
@@ -33,16 +36,34 @@ export function renderVinyl(context : CanvasRenderingContext2D, radius : number)
  * Renders the label of a record.
  */
 export function renderLabel(context : CanvasRenderingContext2D, radius : number) {
-  // Inner circle:
-  context.beginPath();
-  context.arc(radius, radius, radius * 0.35, 0, 2 * Math.PI);
-  context.fillStyle = 'white';
-  context.fill();
+  const image = new Image();
+  image.onload = () => {
+    context.save();
+
+    // Clip circle from label.
+    context.beginPath();
+    context.arc(radius, radius, radius * 0.35, 0, 2 * Math.PI);
+    context.clip();
+    
+    // Drab label.
+    const labelSize = radius * 0.7;
+    context.drawImage(image, radius - (labelSize / 2), radius - (labelSize / 2), labelSize, labelSize);
+
+    context.restore();
+
+    // Spindle
+    context.beginPath();
+    context.arc(radius, radius, radius * 0.03, 0, 2 * Math.PI);
+    context.fillStyle = SPINDLE_COLOR;
+    context.fill();
+  }
+
+  image.src = 'images/faultlines_label.jpg';
 
   // Spindle
   context.beginPath();
   context.arc(radius, radius, radius * 0.03, 0, 2 * Math.PI);
-  context.fillStyle = 'black';
+  context.fillStyle = SPINDLE_COLOR;
   context.fill();
 }
 
@@ -63,9 +84,11 @@ export function renderSlices(context : CanvasRenderingContext2D, radius : number
     const endY = radius * Math.sin(angle);
 
     context.beginPath();
+    context.lineWidth = 3;
     context.moveTo(0, 0);
     context.lineTo(endX, endY);
     context.closePath();
+    context.strokeStyle = DIVIDING_LINE_COLOR;
     context.stroke();
 
     // Label:
